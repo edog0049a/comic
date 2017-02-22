d8ba960e7139ba73de007fc32354d4f8109e1ce1
eli reid <Eli.R@emrelectrical.com>
2017-02-22 17:04:43 -0500

    added comicbook and artist model

---
+++ src/ComicBookGallery/ComicBookGallery2/Models/ComicBooks.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookGallery2.Models
+{
+    class ComicBooks
+    {
+        public int Id { get; set; }
+        public string SeriesTile { get; set; }
+        public int IdIssueNumber { get; set; }
+        public string DescriptionHtml { get; set; }
+        public Artist[] Artists { get; set; }
+        public bool Favorite { get; set; }
+
+    }
+}
