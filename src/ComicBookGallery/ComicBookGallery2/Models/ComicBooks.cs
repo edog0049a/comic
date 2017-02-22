a1e2ab9285bb23150ee76ce36290e34318b47f72
eli reid <Eli.R@emrelectrical.com>
2017-02-22 17:13:23 -0500

    updated comicbook model

--- src/ComicBookGallery/ComicBookGallery2/Models/ComicBooks.cs
+++ src/ComicBookGallery/ComicBookGallery2/Models/ComicBooks.cs
@@ -10,10 +10,27 @@ namespace ComicBookGallery2.Models
     {
         public int Id { get; set; }
         public string SeriesTile { get; set; }
-        public int IdIssueNumber { get; set; }
+        public int IssueNumber { get; set; }
         public string DescriptionHtml { get; set; }
         public Artist[] Artists { get; set; }
         public bool Favorite { get; set; }
+
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTile + " #" + IssueNumber; 
+            }
+        }
+
+        public string CoverImageFileName
+        {
+            get
+            {
+                return SeriesTile.Replace(" ", "-").ToLower() + "-" 
+                        + IssueNumber + ".jpg"; 
+            }
+        }
 
     }
 }
